3abc41f766a1fb49603ed0333d36b299c07ab334
Prabhjot Dev <prabhjotdev@live.com>
2021-04-16 12:41:41 -0400

    Second Commit with all the files
    node created, test created with all dependencies.

---
+++ test.js
@@ -0,0 +1,8 @@
+// write yourself a test case
+// a fake test case
+describe("Adding two numbers", () => {
+    it("Should return the sum", ()=> {
+        expect(10+20).toBe(30)              
+    })
+ })
+ 
